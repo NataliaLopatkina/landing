7504fd4c6047fa2b4472d355ea3557a5a6099964
NataliaLopatkina <51717101+NataliaLopatkina@users.noreply.github.com>
2019-12-20 16:30:19 +0300

    Add slider, validation

--- layout/scripts/common.js
+++ layout/scripts/common.js
@@ -1,1 +1,38 @@
-console.log('Hello, I am Landing!')
+// Slider
+
+$('.slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: true
+});
+
+// Validation form
+
+class Validation {
+    constructor() {
+        this.validationForm();
+    }
+
+    validationForm() {
+        const form = document.querySelector('.subscription__form');
+        const email = document.querySelector('.subscription__field');
+        const message = document.querySelector('.subscription__message');
+
+        form.addEventListener("submit", (event)=> {
+            if (email.validity.patternMismatch) {
+                message.innerHTML = "Введен некорректный E-mail";
+                message.classList.add('subscription__message--error');
+            } else if (email.validity.valueMissing) {
+                message.innerHTML = "E-mail - обязательное поле";
+                message.classList.add('subscription__message--error');
+            } else {
+                message.innerHTML = "Заявка отправлена!";
+                message.classList.remove('subscription__message--error');
+            }
+        });
+    }
+}
+
+const validation = new Validation();
