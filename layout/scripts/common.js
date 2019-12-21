58ada2f77c4d0706e0cf3d21933ad03e74bca35e
Natalia <natalopatkina1994@yandex.ru>
2019-12-21 10:46:50 +0300

    Change validation

--- layout/scripts/common.js
+++ layout/scripts/common.js
@@ -8,34 +8,24 @@ $('.slider').slick({
     pauseOnHover: true
 });
 
-// Validation form
+//Validation form
 
-// class Validation {
-//     constructor() {
-//         this.validationForm();
-//     }
+const form = document.querySelector('.subscription__form');
+const email = document.querySelector('.subscription__field');
+const message = document.querySelector('.subscription__message');
 
-//     validationForm() {
-//         const form = document.querySelector('.subscription__form');
-//         const email = document.querySelector('.subscription__field');
-//         const message = document.querySelector('.subscription__message');
-
-//         form.addEventListener('submit', ()=> {
-//             if (email.validity.patternMismatch) {
-//                 message.innerHTML = 'Введен некорректный E-mail';
-//                 message.classList.add('subscription__message--error');
-//                 email.value = ''
-//             } else if (email.validity.valueMissing) {
-//                 message.innerHTML = 'E-mail - обязательное поле';
-//                 message.classList.add('subscription__message--error');
-//                 email.value = ''
-//             } else {
-//                 message.innerHTML = 'Заявка отправлена!';
-//                 message.classList.remove('subscription__message--error');
-//                 email.value = ''
-//             }
-//         });
-//     }
-// }
-
-// const validation = new Validation();
+form.addEventListener('submit', ()=> {
+    if (email.validity.patternMismatch) {
+        message.innerHTML = 'Введен некорректный E-mail';
+        message.classList.add('subscription__message--error');
+        email.value = ''
+    } else if (email.validity.valueMissing) {
+        message.innerHTML = 'E-mail - обязательное поле';
+        message.classList.add('subscription__message--error');
+        email.value = ''
+    } else {
+        message.innerHTML = 'Заявка отправлена!';
+        message.classList.remove('subscription__message--error');
+        email.value = ''
+    }
+});
